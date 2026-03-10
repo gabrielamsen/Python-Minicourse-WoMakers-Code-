0ca88e57f443d3324c91a7bf3d5c38e1f6da7482
Gabriela <gabrielamatiasena@gmail.com>
2026-03-10 13:03:42 -0300

    aulas 04 e 05

---
+++ aula_04.py
@@ -0,0 +1,16 @@
+#Testing the hashtags for comments in Python
+print("Hi!")
+
+'''
+That's a comment
+of multiple lines
+of which I can write anything I want   
+'''
+name = input("What is your name? ")
+print("Welcome: ", name)
+
+age = input("What is your age? ")
+print("Your age is: ", age)
+
+profession = input("What is your profession? ")
+print("Your profession is: ", profession)
---
+++ aula_05.py
@@ -0,0 +1,9 @@
+#Learning variables
+name = "Gabriela"
+age = 25
+
+print("Name: ", name, "Age: ", age)
+
+#Variables with the same value: a, b and c have the same value, but they are different variables
+a = b = c = 10
+print (a, b, c)
