596329da1baf65e8902f21a952ca62ab8d62c488
Gabriela <gabrielamatiasena@gmail.com>
2026-03-09 21:10:36 -0300

    first commit class 03

---
+++ aula_03.py
@@ -0,0 +1,11 @@
+print("Hi!")
+
+name = input("What is your name? ")
+print("Welcome: ", name)
+
+age = input("What is your age? ")
+print("Your age is: ", age)
+
+profession = input("What is your profession? ")
+print("Your profession is: ", profession)
+
